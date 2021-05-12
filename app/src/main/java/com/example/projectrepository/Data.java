2260501db5453700368765f77085b7b58b1f7d3c
Rebecka <b20rebla@student.his.se>
2021-05-12 15:40:25 +0200

    Skapade en Java klass (Data)

---
+++ app/src/main/java/com/example/projectrepository/Data.java
@@ -0,0 +1,11 @@
+package com.example.projectrepository;
+
+public class Data {
+
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+}
