728f8264e46e55ed9d29ad44b7865473de39a841
Rebecka <b20rebla@student.his.se>
2021-05-12 16:12:30 +0200

    JsonTask och Json web service lades till

--- app/src/main/java/com/example/projectrepository/MainActivity.java
+++ app/src/main/java/com/example/projectrepository/MainActivity.java
@@ -2,23 +2,51 @@ package com.example.projectrepository;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Data> arrayMountain;
     private ArrayAdapter<Data> adapter;
 
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            Log.e("TAG", "Could not read file: " + fileName);
+            return null;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String s = readFile("data.json");
+        Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
+
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -29,5 +57,68 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("TAG", "Starta Om Appen");
             }
         });
+
+        new JsonTask().execute("https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=b20rebla");
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            Log.d("TAG", json);
+
+            Gson gson = new Gson();
+            Data[] tempdata = gson.fromJson(json, Data[].class);
+
+            for (int i = 0; i < tempdata.length; i++) {
+                Data m = tempdata[i];
+                Log.d("TAG", m.toString());
+
+
+
+
+            }
+
+
+
+        }
     }
 }
